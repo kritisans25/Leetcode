bf772480a70a8d7d17e76bfed850a6ce768d0f87
kritisans25 <155572502+kritisans25@users.noreply.github.com>
2026-07-23 21:54:37 +0530

    Time: 3 ms (2.56%), Space: 8.7 MB (11.39%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string decimalToBinary(int n){
+        string res="";
+        while(n>0){
+            if(n%2==1) res=res+'1';
+            else{
+                res=res+'0';
+            }
+            n=n/2;
+        }
+        return res;
+    }
+    int hammingWeight(int n) {
+        string result=decimalToBinary(n);
+        int count=0;
+        for(int i=0;i<result.size();i++){
+            if(result[i]=='1'){
+                count++;
+            }
+        }
+        return count;
+    }
+};
