5c2bf812c3824ddb49d01225a9cffc3dd8d56685
kritisans25 <155572502+kritisans25@users.noreply.github.com>
2026-07-25 23:15:52 +0530

    Time: 168 ms (62.89%), Space: 12.7 MB (65.03%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        vector<bool> prime(n, true);
+
+        prime[0] = false;
+        prime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i]) {
+                // Mark all multiples of i as not prime
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (prime[i])
+                count++;
+        }
+
+        return count;
+    }
+};
