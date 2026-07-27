2146e10110cfbf5387589a11994d7c8e26a55ccd
kritisans25 <155572502+kritisans25@users.noreply.github.com>
2026-07-27 21:10:53 +0530

    Time: 0 ms (100%), Space: 14 MB (65.21%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void newFunction(int index,vector<int> &arr,int target,vector<vector<int>>&ans,vector<int>&ds){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=index;i<arr.size();i++){
+            if(i>index && arr[i]==arr[i-1])continue;
+            if(arr[i]>target)break;
+            ds.push_back(arr[i]);
+            newFunction(i+1,arr,target-arr[i],ans,ds);
+            ds.pop_back();
+        }
+
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>ans;
+        vector<int>ds;
+        newFunction(0,candidates,target,ans,ds);
+        return ans;
+    }
+};
