2f1f3cfa1926f46533abfcb26de169c569822076
kritisans25 <155572502+kritisans25@users.noreply.github.com>
2026-08-02 15:31:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.67%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -5,8 +5,8 @@ public:
         dp[0]=0;
         dp[1]=1;
         dp[2]=1;
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        for(int i=0;i<=n-3;i++){
+            dp[i+3]=dp[i]+dp[i+2]+dp[i+1];
         }
         return dp[n];
     }
